ca12569b9f81340f242928c611b6f410462cadf6
phnz <hello@popcorntime.io>
2014-11-18 17:01:37 -0500

    first commit

---
+++ server/config/secrets.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+  db: process.env.MONGODB || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/stripe-membership',
+
+  sessionSecret: process.env.SESSION_SECRET || '',
+
+  mailgun: {
+    user: process.env.MAILGUN_USER || '',
+    password: process.env.MAILGUN_PASSWORD || ''
+  },
+
+  stripeOptions: {
+    apiKey: process.env.STRIPE_KEY || '',
+    stripePubKey: process.env.STRIPE_PUB_KEY || '',
+    defaultPlan: 'free',
+    plans: ['monthly', 'yearly'],
+    planData: {
+      'monthly': {
+        name: 'Monthly',
+        price: 5
+      },
+      'yearly': {
+        name: 'Yearly',
+        price: 40
+      }
+    }
+  },
+
+  googleAnalytics: process.env.GOOGLE_ANALYTICS || ''
+};
