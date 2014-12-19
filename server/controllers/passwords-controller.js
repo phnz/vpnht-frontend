1be18d57bf0bcfe0af8dfd37b6da76702e9d40dc
phnz <hello@popcorntime.io>
2014-12-19 10:58:18 -0500

    Fix bad email link

--- server/controllers/passwords-controller.js
+++ server/controllers/passwords-controller.js
@@ -126,7 +126,7 @@ exports.postForgotPassword = function (req, res, next) {
 				subject: 'Reset your password on VPN.ht',
 				text: 'You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n' +
 					'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-					'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+					'https://vpn.ht/reset/' + token + '\n\n' +
 					'If you did not request this, please ignore this email and your password will remain unchanged.\n'
 			};
 			transporter.sendMail(mailOptions, function (err) {
