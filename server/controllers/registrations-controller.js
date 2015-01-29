aaba7e13511315e036d63497f5bf7630a07e75d1
phnz <phnz@popcorntime.io>
2015-01-29 10:52:50 -0500

    revert new LP

--- server/controllers/registrations-controller.js
+++ server/controllers/registrations-controller.js
@@ -162,7 +162,7 @@ exports.getSignupPT = function (req, res) {
 	if (errorFlash.length) {
 		error = errorFlash[0];
 	}
-	res.render('signup-popcorntime2', {
+	res.render('signup-popcorntime', {
 		form: form,
 		error: error
 	});
